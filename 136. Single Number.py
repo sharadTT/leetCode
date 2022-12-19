bbb8d29fdb5d4b0ac59eec78561d22fffc02327b
Sharad Tembhurne <36073410+sharadTT@users.noreply.github.com>
2022-12-19 04:23:41 -0500

    Create 136. Single Number.py

---
+++ 136. Single Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return nums[0]
+        nums.sort()
+        i = 0
+        while i < len(nums)-1:
+            if nums[i] != nums[i+1]:
+                return nums[i]
+            i += 2
+
+        return nums[len(nums)-1]
